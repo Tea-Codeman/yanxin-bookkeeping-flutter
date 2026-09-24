cc7e46d62289ae910ff4b5d46dc89983be0f7211
modestyd5 <@723987560@163.com>
2026-09-25 03:45:14 +0800

    chore(profile): 版本角标 v0.7.9 → v0.7.10

--- lib/features/profile/presentation/profile_page.dart
+++ lib/features/profile/presentation/profile_page.dart
@@ -243,7 +243,7 @@ class _BrandTip extends StatelessWidget {
               ),
               Spacer(),
               Text(
-                'v0.7.9',
+                'v0.7.10',
                 style: TextStyle(
                   fontSize: 12,
                   fontWeight: FontWeight.w800,
